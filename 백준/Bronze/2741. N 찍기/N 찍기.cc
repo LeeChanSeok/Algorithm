0c07bb465b2cf3d92055022e362b22f698a4d43a
Chanseok <ckstjr93@naver.com>
2022-08-01 22:41:31 +0900

    [Bronze V] Title: N 찍기, Time: 24 ms, Memory: 1524 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    
+    int N,i;
+    cin>>N;
+    
+    for (i=1;i<=N;i++)
+    {
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
