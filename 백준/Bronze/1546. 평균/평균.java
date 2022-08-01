03fcb5860e5e26d541dfb5c97ca22fa9192f0bcd
Chanseok <ckstjr93@naver.com>
2022-08-01 22:08:45 +0900

    [Bronze I] Title: 평균, Time: 156 ms, Memory: 16460 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] score = new int[N];
+		int max = 0;
+		for(int i = 0; i < N; i++) {
+			score[i] = sc.nextInt();
+			if (score[i] > max) max = score[i];
+		}
+		
+		double sum = 0;
+		for(int i = 0; i < N; i++) {
+			sum += (double)score[i]/max*100;
+		}
+		
+		System.out.println(sum/N);
+	}
+
+}
