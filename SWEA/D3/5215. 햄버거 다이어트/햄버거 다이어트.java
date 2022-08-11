a43585765e3649200d3d0865185acf9f0b829826
Chanseok <ckstjr93@naver.com>
2022-08-11 13:27:37 +0900

    [D3] Title: 햄버거 다이어트, Time: 264 ms, Memory: 28,176 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,21 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+class Ingredient {
+	int score, cal;
+	double rate;
+
+	public Ingredient(int score, int cal) {
+		this.score = score;
+		this.cal = cal;
+		this.rate = (double) score / cal;
+	};
+
+}
 
 public class Solution {
 	public static int max;
-	
-	public static void combination(int[][] ingredient, int N, int L, int cnt, int score, int cal) {
-		if(cal > L) return;
-		if(cnt == N) {
+	public static PriorityQueue<Ingredient> pq;
+	public static void combination(List<Ingredient> list, int N, int L, int cnt, int score, int cal) {
+		if (cal > L)
+			return;
+		if (cnt == N) {
 			max = Math.max(max, score);
 			return;
 		}
-		combination(ingredient, N, L, cnt+1, score, cal);
-		combination(ingredient, N, L, cnt+1, score + ingredient[cnt][0], cal + ingredient[cnt][1]);
+		combination(list, N, L, cnt + 1, score+ list.get(cnt).score, cal + list.get(cnt).cal);
+		combination(list, N, L, cnt + 1, score, cal);
+
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -31,16 +48,19 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			L = Integer.parseInt(st.nextToken());
 			
-			ingredient = new int[N][2];
+			pq = new PriorityQueue<Ingredient>(
+					(a, b) -> Double.compare(b.rate, a.rate)
+					);
 			
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				ingredient[i][0] = Integer.parseInt(st.nextToken());
-				ingredient[i][1] = Integer.parseInt(st.nextToken());
+				pq.add(new Ingredient(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 			}
 			
 			max = Integer.MIN_VALUE;
-			combination(ingredient, N, L, 0, 0, 0);
+			//Ingredient[] list = (Ingredient[])pq.toArray();
+			List<Ingredient> list = new ArrayList<Ingredient>(pq);
+			combination(list, N, L, 0, 0, 0);
 			System.out.printf("#%d %d\n",tc, max);
 			
 		} // test-case 종료
