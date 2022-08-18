5712d30b8abf0b029bcedc1d0570a0ac2f5a9794
Chanseok <ckstjr93@naver.com>
2022-08-18 22:02:15 +0900

    [Gold IV] Title: 알파벳, Time: 128 ms, Memory: 12060 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,39 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int R, C;
-	static int max;
-	static int[] dx = { 0, 1, 0, -1 };
-	static int[] dy = { 1, 0, -1, 0 };
-	static char[][] map;
-
+	static int R, C, ans;
+	static char[][] board;
+    static int[][] visit;
+	static final int[] dx = {0,0,1,-1};
+	static final int[] dy = {1,-1,0,0};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-
-		map = new char[R][C];
-		for (int i = 0; i < R; i++)
-			map[i] = br.readLine().toCharArray();
-
-		max = 0;
-		dfs(0, 0, 1 << map[0][0] - 'A', 1);
-		System.out.println(max);
+		board = new char[R][C];
+        visit = new int[R][C];
+		for(int i=0; i<R; ++i) {
+			board[i] = br.readLine().toCharArray();
+		}
+        
+		dfs(0,0,1<<board[0][0]-'A',1);
+		System.out.println(ans);
 	}
-
-	static void dfs(int x, int y, int flag, int move) {
-		
-		max = Math.max(max, move);
-
-		for (int d = 0, dSize = dx.length; d < dSize; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C || (flag & 1 << map[nx][ny] - 'A') != 0)	continue;
-			dfs(nx, ny, flag | 1 << map[nx][ny] - 'A', move+1);
+	
+	static void dfs(int x, int y, int bit, int cnt) {
+        if(visit[x][y] == bit) return;
+        visit[x][y] = bit;
+		ans = Math.max(ans, cnt);
+		for(int i = 0; i<4; ++i) {
+			int nx = x+dx[i];
+			int ny = y+dy[i];
+			if(nx<0||ny<0||nx==R||ny==C||(bit&(1<<board[nx][ny]-'A'))!=0) continue;
+			dfs(nx, ny, bit|(1<<board[nx][ny]-'A'), cnt+1);
 		}
 	}
 }
