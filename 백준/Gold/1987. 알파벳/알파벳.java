bc053da9e94db3d8160b2e3cdfb7fc45c9a31970
Chanseok <ckstjr93@naver.com>
2022-08-18 21:54:51 +0900

    [Gold IV] Title: 알파벳, Time: 832 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,44 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Main {
 	static int R, C;
 	static int max;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
 	static char[][] map;
-	static Set<Character> alpabet;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 		R = Integer.parseInt(input[0]);
 		C = Integer.parseInt(input[1]);
-		
+
 		map = new char[R][C];
-		for(int i = 0; i < R; i++)
+		for (int i = 0; i < R; i++)
 			map[i] = br.readLine().toCharArray();
-		
+
 		max = 0;
-		alpabet = new HashSet<>();
-		alpabet.add(map[0][0]);
-		dfs(0, 0, 1, alpabet);
+		dfs(0, 0, 1 << map[0][0] - 'A', 1);
 		System.out.println(max);
 	}
-	
-	public static void dfs(int x, int y, int move, Set<Character> alpabet) {
+
+	public static void dfs(int x, int y, int flag, int move) {
 		
-		for(int d = 0, dSize = dx.length; d < dSize; d++) {
+		max = Math.max(max, move);
+
+		for (int d = 0, dSize = dx.length; d < dSize; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
-			if(nx < 0 || nx >= R || ny < 0 || ny >=C || alpabet.contains(map[nx][ny])) continue;
-			alpabet.add(map[nx][ny]);
-			dfs(nx, ny, move+1, alpabet);
-			alpabet.remove(map[nx][ny]);
+			if (nx < 0 || nx >= R || ny < 0 || ny >= C || (flag & 1 << map[nx][ny] - 'A') != 0)	continue;
+			dfs(nx, ny, flag | 1 << map[nx][ny] - 'A', move+1);
 		}
-		max = Math.max(max, move);
 	}
 }
