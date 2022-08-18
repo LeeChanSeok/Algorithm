226057edf16107ae216eddbeaf3c8114c8cc54ac
Chanseok <ckstjr93@naver.com>
2022-08-18 21:58:51 +0900

    [Gold IV] Title: 알파벳, Time: 792 ms, Memory: 12144 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int R, C;
@@ -15,9 +12,9 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		R = Integer.parseInt(input[0]);
-		C = Integer.parseInt(input[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 
 		map = new char[R][C];
 		for (int i = 0; i < R; i++)
@@ -28,7 +25,7 @@ public class Main {
 		System.out.println(max);
 	}
 
-	public static void dfs(int x, int y, int flag, int move) {
+	static void dfs(int x, int y, int flag, int move) {
 		
 		max = Math.max(max, move);
 
