b49aa0a5fee566c308eb5f5c87281c6786013989
Chanseok <ckstjr93@naver.com>
2022-08-22 13:27:56 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 12460 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +11,7 @@ public class Main {
 	public static int L, C;
 	public static int cnt, idx, vCnt, cCnt;
 	public static char[] input, pick;
-	public static boolean[] isSelected;
-	public static StringBuffer sb;
+	public static StringBuilder sb;
 	public static List<Character> vowels = new ArrayList<>(5);
 	
 	public static void combination(int cnt, int idx, int vCnt, int cCnt) {
@@ -20,20 +21,17 @@ public class Main {
 		}
 		
 		for(int i = idx; i < C; i++) {
-			if(isSelected[i]) continue;
-			isSelected[i] = true;
-			pick[cnt] = input[i];
-			
+			pick[cnt] = input[i];			
 			if(vowels.contains(input[i])) combination(cnt+1, i+1, vCnt+1, cCnt);
 			else combination(cnt+1, i+1, vCnt, cCnt+1);
-			isSelected[i] = false;
 		}
 		
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuffer();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
 		
 		vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
 		
@@ -43,12 +41,12 @@ public class Main {
 		
 		input = br.readLine().replace(" ", "").toCharArray();
 		pick = new char[L];
-		isSelected = new boolean[C];
 		
 		Arrays.sort(input);
 		
 		combination(0, 0, 0, 0);
-		System.out.println(sb.toString());
+		bw.write(sb.toString());
+		bw.close();
 	}
 
 }
