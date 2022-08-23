f36804008f3082320ed6999225c0c9b3eb08df35
Chanseok <ckstjr93@naver.com>
2022-08-23 09:54:19 +0900

    [Gold V] Title: ABCDE, Time: 228 ms, Memory: 19020 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
-	static Map<Integer, List<Integer>> graph;
+	//static Map<Integer, List<Integer>> graph;
+	static List<Integer>[] graph;
 	static boolean[] visit;
 	
 	public static void main(String[] args) throws IOException {
@@ -23,24 +22,23 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		graph = new HashMap<>();		
+		graph = new List[N];		
+		for(int i = 0; i < N; i++)
+			graph[i] = new ArrayList<Integer>();
+		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			
-			if(!graph.containsKey(from)) graph.put(from, new ArrayList<>());
-			if(!graph.containsKey(to)) graph.put(to, new ArrayList<>());
-			
-			graph.get(from).add(to);
-			graph.get(to).add(from);
+			graph[from].add(to);
+			graph[to].add(from);
 		}
 		
 		int ans = 0;
+		visit = new boolean[N];
 		for(int v = 0; v < N; v++) {
-			visit = new boolean[N];
-			if(!graph.containsKey(v)) continue;
 			if(dfs(v, 1)) {
 				ans = 1;
 				break;
@@ -56,7 +54,7 @@ public class Main {
 		if(length == 5) return true;
 		visit[v] = true;
 		
-		List<Integer> vertexs = graph.get(v);
+		List<Integer> vertexs = graph[v];
 		
 		for(int nv : vertexs) {
 			if(!visit[nv]) 
