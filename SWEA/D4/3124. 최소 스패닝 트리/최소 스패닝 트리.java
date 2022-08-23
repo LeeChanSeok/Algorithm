040c3e3430e871e0574108d63d81c9637a93cbb8
Chanseok <ckstjr93@naver.com>
2022-08-23 14:39:25 +0900

    [D4] Title: 최소 스패닝 트리, Time: 4,424 ms, Memory: 221,944 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,88 +3,89 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static class Edge implements Comparable<Edge>{
-		int a, b, c;
+	static class Edge implements Comparable<Edge> {
+		int w, to;
 
-		public Edge(int a, int b, int c) {
+		public Edge(int w, int to) {
 			super();
-			this.a = a;
-			this.b = b;
-			this.c = c;
+			this.w = w;
+			this.to = to;
 		}
 
 		@Override
 		public int compareTo(Edge o) {
-			return Integer.compare(this.c, o.c);
+			return Integer.compare(this.w, o.w);
 		}
-		
+
 	}
-	
+
 	static int V, E;
-	static int[] P;
-	static Edge[] edges;
-	
+	static List<Edge>[] graph;
+	static boolean[] visit;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++){
+		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
-			
-			P = new int[V + 1];
-			for(int i = 1; i < V + 1; i++)
-				P[i] = i;
-			
-			edges = new Edge[E];
-			for(int i = 0; i < E; i++) {
+
+			graph = new List[V + 1];
+			visit = new boolean[V + 1];
+
+			for (int i = 0; i < V + 1; i++)
+				graph[i] = new ArrayList<>();
+
+			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-				
-				edges[i] = new Edge(a, b, c); 
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				int weight = Integer.parseInt(st.nextToken());
+
+				graph[from].add(new Edge(weight, to));
+				graph[to].add(new Edge(weight, from));
+
 			}
-			
-			Arrays.sort(edges);
-			int cnt = 0;
+
 			long res = 0;
-			for(Edge edge : edges) {
-				if(union(edge.a, edge.b)) {
-					res += edge.c;
-					if(++cnt == V-1) break;
+			int cnt = 1;
+			int vertex = 1;
+			visit[vertex] = true;
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			for (Edge e : graph[vertex])
+				pq.add(e);
+
+			while (!pq.isEmpty()) {
+
+				Edge cur = pq.poll();
+				if (!visit[cur.to]) {
+					visit[cur.to] = true;
+					res += cur.w;
+					if(++cnt == V) break;
+					for (Edge e : graph[cur.to])
+						pq.add(e);
 				}
+
 			}
-			
+
 			sb.append("#" + tc + " " + res + "\n");
-		}// test-case 종료
+		} // test-case 종료
 		bw.write(sb.toString());
 		bw.close();
-		
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		
-		if(aRoot == bRoot) return false;
-		P[bRoot] = aRoot;
-		return true;
-	}
-	
-	static int find(int x) {
-		if(x == P[x]) return x;
-		return P[x] = find(P[x]);
+
 	}
 
 }
