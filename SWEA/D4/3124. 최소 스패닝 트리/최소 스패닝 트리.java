05a330dfcb394284d1a976284b5647637d310d76
Chanseok <ckstjr93@naver.com>
2022-08-25 12:59:52 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,758 ms, Memory: 121,112 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,32 +4,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-	static class Edge implements Comparable<Edge> {
-		int w, to;
-
-		public Edge(int w, int to) {
+	
+	static class Node{
+		int vertex, weight;
+		Node next;
+		
+		public Node(int vertex, int weight, Node next) {
 			super();
-			this.w = w;
-			this.to = to;
+			this.vertex = vertex;
+			this.weight = weight;
+			this.next = next;
 		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.w, o.w);
-		}
-
 	}
+	
+	static class Vertex{
+		int no, weight;
 
-	static int V, E;
-	static List<Edge>[] graph;
-	static boolean[] visit;
-
+		public Vertex(int no, int weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
+		}
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -39,48 +43,57 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-
-			graph = new List[V + 1];
-			visit = new boolean[V + 1];
-
-			for (int i = 0; i < V + 1; i++)
-				graph[i] = new ArrayList<>();
-
-			for (int i = 0; i < E; i++) {
+			
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			
+	
+			Node[] adjList = new Node[V+1];
+			
+			for(int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
+				
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				int weight = Integer.parseInt(st.nextToken());
-
-				graph[from].add(new Edge(weight, to));
-				graph[to].add(new Edge(weight, from));
-
+				
+				
+				adjList[from] = new Node(to, weight, adjList[from]);
+				adjList[to] = new Node(from, weight, adjList[to]);
+				
 			}
-
+			
+			int[] minEdge = new int[V+1];
+			boolean[] visited = new boolean[V+1];
+			
+			Arrays.fill(minEdge, Integer.MAX_VALUE);
+			minEdge[1] = 0;
 			long res = 0;
-			int cnt = 1;
-			int vertex = 1;
-			visit[vertex] = true;
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
-			for (Edge e : graph[vertex])
-				pq.add(e);
 
-			while (!pq.isEmpty()) {
+			
+			PriorityQueue<Vertex> pq = new PriorityQueue<>((v1, v2) -> Integer.compare(v1.weight, v2.weight));
+			pq.offer(new Vertex(1, 0));
+			
+			int cnt = 0;
+			while(true) {
+				Vertex cur = pq.poll();
+				if(visited[cur.no]) continue;
+				
+				visited[cur.no] = true;
+				res += cur.weight;
 
-				Edge cur = pq.poll();
-				if (!visit[cur.to]) {
-					visit[cur.to] = true;
-					res += cur.w;
-					if(++cnt == V) break;
-					for (Edge e : graph[cur.to])
-						pq.add(e);
+				if(++cnt == V) break;
+				
+				for(Node temp = adjList[cur.no]; temp != null; temp = temp.next ) {
+					if(!visited[temp.vertex] && minEdge[temp.vertex] > temp.weight) {
+						minEdge[temp.vertex] = temp.weight;
+						pq.offer(new Vertex(temp.vertex, minEdge[temp.vertex]));
+						
+					}
 				}
-
+				
 			}
-
+			
 			sb.append("#" + tc + " " + res + "\n");
 		} // test-case 종료
 		bw.write(sb.toString());
