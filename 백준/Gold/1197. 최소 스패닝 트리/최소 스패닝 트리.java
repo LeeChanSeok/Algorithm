c992ac0fddd8ebd6016883036fa90fac7203be73
Chanseok <ckstjr93@naver.com>
2022-08-26 15:36:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 564 ms, Memory: 53284 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,79 +2,89 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Edge implements Comparable<Edge>{
-		int from, to, weight;
+	static class Node {
+		int vertex, weight;
+		Node next;
 
-		public Edge(int from, int to, int weight) {
+		public Node(int vertex, int weight, Node next) {
 			super();
-			this.from = from;
-			this.to = to;
+			this.vertex = vertex;
 			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
+			this.next = next;
 		}
 	}
-	
+
+	static class Vertex {
+		int no, weight;
+
+		public Vertex(int no, int weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
+		}
+	}
+
 	static int V, E;
-	static int[] P;
-	
+	static final int INF = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
-		
-		Edge[] edges = new Edge[E];
-		for(int i = 0; i < E; i++) {
+
+		Node[] adjList = new Node[V + 1];
+
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
-			
-			edges[i] = new Edge(from, to, weight);
+
+			adjList[from] = new Node(to, weight, adjList[from]);
+			adjList[to] = new Node(from, weight, adjList[to]);
 		}
-		P = new int[V+1];
-		for(int i = 1; i <= V ; i++)
-			P[i] = i;
+
 		
-		Arrays.sort(edges);
+		PriorityQueue<Vertex> pq = new PriorityQueue<>((v1, v2) -> Integer.compare(v1.weight, v2.weight));
+		
+		int[] minEdge = new int[V+1];
+		Arrays.fill(minEdge, INF);
+		
+		boolean[] visited = new boolean[V+1];
+		
+		pq.offer(new Vertex(1, 0));
 		
 		int cnt = 0;
 		int res = 0;
-		
-		for(Edge edge : edges) {
-			if(union(edge.from, edge.to)) {
-				res += edge.weight;
-				if(++cnt == V-1) break;
+		while(true) {
+			
+			Vertex cur = pq.poll();
+			if(visited[cur.no] || minEdge[cur.no] < cur.weight) continue;
+			
+			visited[cur.no]= true;
+			res += cur.weight;
+			
+			if(++cnt == V) break;
+			
+			for(Node temp = adjList[cur.no]; temp != null; temp = temp.next) {
+				if(!visited[temp.vertex] && minEdge[temp.vertex] > temp.weight) {
+					minEdge[temp.vertex] = temp.weight;
+					pq.offer(new Vertex(temp.vertex, temp.weight));
+				}
 			}
+			
 		}
 		
 		System.out.println(res);
 
-	}
-
-	private static boolean union(int a, int b) {
-
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot == bRoot) return false;
-		
-		P[bRoot] = aRoot;
-		return true;
-	}
-
-	private static int find(int x) {
-		if(x == P[x]) return x;
-		return P[x] = find(P[x]);
 	}
 
 }
