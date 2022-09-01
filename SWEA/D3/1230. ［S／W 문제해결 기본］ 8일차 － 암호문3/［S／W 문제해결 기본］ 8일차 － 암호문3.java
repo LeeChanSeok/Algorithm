985622a87edec64fe69f1615a564a5731804c9ad
Chanseok <ckstjr93@naver.com>
2022-09-01 21:54:18 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 165 ms, Memory: 31,020 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static final int T = 10;
+	static int N, M;
+	static List<Integer> pw;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			pw = new ArrayList<>(N);
+			for (int i = 0; i < N; i++)
+				pw.add(Integer.parseInt(st.nextToken()));
+
+			M = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < M; i++) {
+				char command = st.nextToken().charAt(0);
+
+				if (command == 'I') {
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+
+					List<Integer> Add = new ArrayList<>(y);
+					while (y-- > 0)
+						Add.add(Integer.parseInt(st.nextToken()));
+					pw.addAll(x, Add);
+				} else if (command == 'D') {
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+
+					while (y-- > 0)
+						pw.remove(x);
+				} else {
+					int y = Integer.parseInt(st.nextToken());
+					
+					List<Integer> Add = new ArrayList<>(y);
+					while (y-- > 0)
+						Add.add(Integer.parseInt(st.nextToken()));
+					pw.addAll(pw.size() - 1, Add);
+				}
+			}
+
+			sb.append("#" + tc);
+			for (int i = 0; i < 10; i++)
+				sb.append(" " + pw.get(i));
+			sb.append("\n");
+
+		}
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+}
