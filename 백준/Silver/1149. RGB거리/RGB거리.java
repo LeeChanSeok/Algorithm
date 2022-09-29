6024323d9fb010c78baee7da314982b047f5c599
Chanseok <ckstjr93@naver.com>
2022-09-29 16:09:04 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12100 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,50 +5,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N;
-	static int[][] fees, dp;
-	static int INF = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		N = Integer.parseInt(br.readLine());
-		fees = new int[N][3];
+		int N = Integer.parseInt(br.readLine());
+		int[][] fees = new int[N][3];
+		int[][] dp = new int[N][3];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < 3; j++)
 				fees[i][j] = Integer.parseInt(st.nextToken());
 		}
+
+		dp[0][0] = fees[0][0];
+		dp[0][1] = fees[0][1];
+		dp[0][2] = fees[0][2];
 		
-		dp = new int[N][3];
-		for(int i = 0;  i < N; i++) Arrays.fill(dp[i], INF);
-		
-		for(int i = 0 ; i < 3; i++) {
-			dfs(0, i);
+		for(int i = 1; i < N; i++) {
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + fees[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + fees[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + fees[i][2];
 		}
-		int min = INF;
+		
+		int min = Integer.MAX_VALUE;
 		for(int i = 0; i < 3; i++)
-			min = Math.min(min, dp[0][i]);
+			min = Math.min(min, dp[N-1][i]);
+		
 		System.out.println(min);
-	}
-	private static int dfs(int cnt, int color) {
-		
-		if(cnt == N) return 0;
-		if(dp[cnt][color] != INF) return dp[cnt][color];
-		
-		for(int i = 0; i < 3; i++) {
-			if(color == i) continue;
-			
-			int fee = dfs(cnt+1, i);
-			if(dp[cnt][color] > fee + fees[cnt][color]) {
-				dp[cnt][color] = fee + fees[cnt][color];
-			}
-		}
-		
-		return dp[cnt][color];
 	}
 
 }
