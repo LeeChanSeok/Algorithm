0e698cc8bd2bc8d41a5ba7bb9f5b7cbb8bbd34c3
Chanseok <ckstjr93@naver.com>
2022-10-01 00:19:41 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11756 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,58 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static int[] dx = {0, 1, 1};
-	public static int[] dy = {1, 1, 0};
-	public static int map[][];
-	public static int count, N;
-	
-	public static void recursive(int sx, int sy, int dir) {
-		//경계 밖을 나갔을 경우
-		if(sx < 1 || sx >N || sy < 1|| sy > N) return;
-		
-		//벽이 있는 경우
-		if(map[sx][sy] == 1) return;
-		
-		// 대각선의 경우 추가 범위의 벽 확인
-		if(dir==1) {
-			if(map[sx-1][sy] == 1 || map[sx][sy-1]==1) return;
-		}
-		
-		//목적지에 도착하였을 경우
-		if(sx == N && sy == N) {
-			count++;
-			return;
-		}
-		
-		if (dir - 1 >= 0) recursive(sx + dx[dir - 1], sy + dy[dir - 1], dir - 1);
-		recursive(sx + dx[dir], sy + dy[dir], dir);
-		if (dir + 1 <= 2) recursive(sx + dx[dir + 1], sy + dy[dir + 1], dir + 1);	
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int T = 1;
-		for(int tc = 1; tc <= T; tc++) {
-			N = sc.nextInt();
-			
-			map = new int[N+2][N+2];
-			
-			for(int i = 1; i <= N; i++) {
-				for(int j = 1; j <= N; j++) {
-					map[i][j] = sc.nextInt();
-				}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
-			
-			int sx = 1;
-			int sy = 2;
-			int dir = 0;
-			count = 0;
-			
-			recursive(sx, sy, dir);
-			
-			System.out.println(count);			
 		}
+
+		int[][][] dp = new int[N][N][3];
+		
+		for(int j = 1; j < N; j++) {
+			if(map[0][j] == 1) break;
+			dp[0][j][0] = 1;
+		}
+		
+		for(int i = 1; i < N; i++) {
+			for(int j = 2; j < N; j++) {
+				if(map[i][j] == 1) continue;
+				if(map[i][j-1] != 1) dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1];
+				if(map[i-1][j-1] != 1 && map[i-1][j] != 1 && map[i][j-1] != 1) dp[i][j][1] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+				if(map[i-1][j] != 1) dp[i][j][2] = dp[i-1][j][2] + dp[i-1][j][1];
+			}
+		}
+		
+		int sum = 0;
+		for(int d = 0; d < 3; d++) sum += dp[N-1][N-1][d];
+			
+		System.out.println(sum);
 	}
+
 }
