ff264096a99a2f2d993519626a75a4c5eee11cd3
Chanseok <ckstjr93@naver.com>
2022-10-02 15:31:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 267 ms, Memory: 98,448 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -1,116 +1,139 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-
+	
 	static class Cell implements Comparable<Cell>{
-		int x, y, k, life;
-		boolean isActive;
+		int x, y, life, time;
 
-		public Cell(int x, int y, int k, int life, boolean isActive) {
+		public Cell(int x, int y, int life, int time) {
 			super();
 			this.x = x;
 			this.y = y;
-			this.k = k;
 			this.life = life;
-			this.isActive = isActive;
+			this.time = time;
 		}
 
 		@Override
-		public int compareTo(Cell o) {	// 생명력 수치가 높은 줄기 세포를 최대로하는 max Heap
-			return o.k - this.k;
+		public int compareTo(Cell o) {
+			return o.life - this.life;
 		}
 		
 	}
 
-	static int N, M, K;
-	static boolean[][] visit;
-	static Queue<Cell> q;
-
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
+	static List<Cell> nActiveL, ActiveL;
+	static PriorityQueue<Cell> pq;
 
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		
+		int N, M, K;
+		int maxNM = 400;
+		boolean[][] map;
 		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-
+		
+		for(int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-
+				
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-
-			q = new ArrayDeque<>();
-
-			visit = new boolean[N + K][M + K];	// k시간동안 최대 한방향으로 k/2반큼 이동 가능
-			for (int i = 0; i < N; i++) {
+			
+			map = new boolean[400][400];
+			ActiveL = new LinkedList<>();
+			nActiveL = new LinkedList<>();
+			
+			int x, y, life;
+			for(int i = 0; i < N; ++i) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < M; j++) {
-					int num = Integer.parseInt(st.nextToken());
-					if (num == 0)	continue;
-
-					visit[i + K / 2][j + K / 2] = true;		
-					q.add(new Cell(i + K / 2, j + K / 2, num, num, false));
+				for(int j = 0; j < M; ++j) {
+					life = Integer.parseInt(st.nextToken());
+					if(life == 0) continue;
+					
+					x = i + 165;
+					y = j + 165;
+					
+					nActiveL.add(new Cell(x, y, life, 0));
+					map[x][y] = true;
+					
 				}
 			}
+			
+			System.out.printf("#%d %d\n",tc, solution(ActiveL, nActiveL, map, N, M, K));
+			
+		}// tc 종료
+		
+	}
 
-			// simulation
-			PriorityQueue<Cell> pq;	//생명력 수치가 높은 줄기 세포를 우선으로 선택하기 위해서
-			for (int t = 0; t < K; t++) {
+	private static int solution(List<Cell> ActiveL, List<Cell> nActiveL, boolean[][] map, int N, int M, int K) {
+
+		final int[] dx = {-1, 0, 1, 0};
+		final int[] dy = {0, 1, 0, -1};
+		
+		Cell cell;
+		pq = new PriorityQueue<>();
+		ListIterator<Cell> iter;
+		int nx, ny;
+		
+		while(K-- > 0) {
+			
+			// 1. 비활성 상태의 세포들의 시간을 증가시킨다
+			// x시간이 지난 후, 번식을 위해 pq 담고 active queue에 담는다.
+			iter = nActiveL.listIterator();
+			while(iter.hasNext()) {
+				cell = iter.next();
+				if(cell.life == cell.time) {
+					pq.add(cell);
+					ActiveL.add(new Cell(cell.x, cell.y, cell.life, 1));
+					iter.remove();
+				}else {
+					++cell.time;
+				}
+			}
+			
+			// 2. 활성 상태의 세포들의 시간을 증가시킨다
+			// x시간이 지난 후, 삭제시킨다
+			iter = ActiveL.listIterator();
+			while(iter.hasNext()) {
+				cell = iter.next();
+				if(cell.life == cell.time) {
+					iter.remove();
+				}else {
+					++cell.time;
+				}
+			}
+			
+			// 3. 번식할 cell들을 번식 후, nActiveL에 담는다.
+			while(!pq.isEmpty()) {
+				cell = pq.poll();
 				
-				pq = new PriorityQueue<>();
-				for (int s = 0, qSize = q.size(); s < qSize; s++) {
-					Cell cur = q.poll();
+				for(int d = 0; d <4; ++d) {
+					nx = cell.x + dx[d];
+					ny = cell.y + dy[d];
 					
-					if(cur.isActive && cur.k == cur.life) {	// 활성화된 줄기 세포는 첫 1시간 동안 상, 하, 좌, 우 네 방향으로 동시에 번식
-						for (int d = 0, dSize = dx.length; d < dSize; d++) {
-							int nx = cur.x + dx[d];
-							int ny = cur.y + dy[d];
-							if (visit[nx][ny])	continue;
-							pq.add(new Cell(nx, ny, cur.k, cur.k, false));
-						}
+					if(!map[nx][ny]) {
+						map[nx][ny] = true;
+						nActiveL.add(new Cell(nx, ny, cell.life, 0));
 					}
 					
-					--cur.life;
-					if(cur.life == 0) {
-						if(cur.isActive) continue;	// 활성 상태가 되면 X시간 동안 살아있을 수 있으며 X시간이 지나면 세포는 죽게 된다.
-						else {
-							cur.isActive = true;	// X시간 동안 비활성 상태이고 X시간이 지나는 순간 활성 상태
-							cur.life = cur.k;							
-						}
-					}					
-					q.add(cur);
-					
 				}
 				
-				//생명력 수치가 높은 줄기 세포가 해당 그리드 셀을 혼자서 차지
-				while(!pq.isEmpty()) {
-					Cell next = pq.poll();
-					if (visit[next.x][next.y])	continue;
-					visit[next.x][next.y] = true;
-					q.add(new Cell(next.x, next.y, next.k, next.k, next.isActive));
-				}
 			}
-			// end - simulation
-			sb.append("#" + tc + " " + q.size() + "\n");
-
-		} // test-case 종료
-
-		bw.write(sb.toString());
-		bw.close();
-
+			
+		}
+		
+		return ActiveL.size() + nActiveL.size();
 	}
+
 
 }
