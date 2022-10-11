0388cf86bc6aed1f0b71cd1b6ad817e5bbf80693
Chanseok <ckstjr93@naver.com>
2022-10-11 12:42:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 132 ms, Memory: 32,816 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -8,13 +9,11 @@ import java.util.StringTokenizer;
 public class Solution {
 	static int N, X;
 	static int ans;
-	static int[][] map;
+	static int[][] board;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
@@ -23,119 +22,152 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			X = Integer.parseInt(st.nextToken());
 
-			map = new int[N][N];
+			board = new int[N][N];
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++)
-					map[i][j] = Integer.parseInt(st.nextToken());
+					board[i][j] = Integer.parseInt(st.nextToken());
 			}
 			
-			ans = solution();
-			sb.append("#" + tc + " " + ans + "\n");
-
+			ans = solution(N, X, board);
+			System.out.printf("#%d %d\n", tc, ans);
 		} // test-case 종료
-
-		bw.write(sb.toString());
-		bw.close();
 
 	}
 
-	private static int solution() {
-		int res = 0;
-		for (int i = 0; i < N; i++) {
-			int num = map[i][0];
-			int cnt = 1;
-			boolean isBuild = true;	// 첫 시작
-			boolean isUp = false, isDown = true;
-			for (int j = 1; j < N; j++) {
-				if(map[i][j] == num) {
-					cnt++;
-					if(cnt >= X &&!isDown && !isUp) {
-						isDown = true;
-						cnt = 0;
-						isBuild = true;
-					}else if(cnt >= X && isDown && !isUp) {
-						isUp = true;		
-						isBuild = true;
+	private static int solution(int N, int X, int[][] board) {
+		boolean[][][] build = new boolean[N][N][2];
+		
+		int ans = 0;
+		int k, h, cnt;
+		boolean isBuild;
+		// 행
+		
+		for(int i = 0; i < N; ++i) {
+			isBuild = true;
+			h = board[i][0];
+			
+			for(int j = 1; j < N; ++j) {
+				if(board[i][j] == h) continue;
+				
+				// 높아지는 경우
+				else if(board[i][j] > h) {
+					// 높이 차가 2 이상인 경우
+					if(board[i][j] - h > 1) {
+						isBuild = false; 
+						break;
 					}
 					
-				}else if(map[i][j] > num){ // 높이가 높아지는 경우
+					k = j - 1;
+					cnt = 0;
+					// j-1부터 뒤쪽에 L개의 동일 높이의 땅이 있는지 확인
+					while(k >= 0 && board[i][k] == h && !build[i][k][0]) {
+						++cnt;
+						--k;
+					}
 					
-					if(map[i][j] - num > 1 || !isUp) {
+					if(cnt < X) {
 						isBuild = false;
 						break;
 					}
-					isDown = true;
-					isUp = false;
 					
-					cnt = 1;
-					num = map[i][j];
-					isBuild = true;		
-					
-				}else {	// 높이가 낮아지는 경우
-					if(num - map[i][j] > 1 ||!isDown) {
-						isBuild = false;
-						break;
-					}
-					isDown = false;
-					isUp = false;
-					
-					cnt = 1;
-					num = map[i][j];
-					isBuild = false;
+					// 경사로 설치
+					for(k = j - X; k < j; ++k) build[i][k][0] = true;
+					h = board[i][j];
 				}
 				
-			}
-			
-			if(isBuild) res++;
-		}
-		
-		for (int j = 0; j < N; j++) {
-			int num = map[0][j];
-			int cnt = 1;
-			boolean isBuild = true;	// 첫 시작
-			boolean isUp = false, isDown = true;
-			for (int i = 1; i < N; i++) {
-				if(map[i][j] == num) {
-					cnt++;
-					if(cnt >= X &&!isDown && !isUp) {
-						isDown = true;
-						cnt = 0;
-						isBuild = true;
-					}else if(cnt >= X && isDown && !isUp) {
-						isUp = true;		
-						isBuild = true;
+				// 낮아지는 경우
+				else if(board[i][j] < h) {
+					// 높이 차가 2 이상인 경우
+					if(h - board[i][j] > 1) {
+						isBuild = false; 
+						break;
+					}
+					k = j;
+					cnt = 0;
+					// j부터 뒤쪽에 L개의 동일 높이의 땅이 있는지 확인
+					while(k < N && board[i][k] == h-1 && !build[i][k][0]) {
+						++cnt;
+						++k;
 					}
 					
-				}else if(map[i][j] > num){ // 높이가 높아지는 경우
-					if(map[i][j] - num > 1 || !isUp) {
+					if(cnt < X) {
 						isBuild = false;
 						break;
 					}
-					isDown = true;
-					isUp = false;
 					
-					cnt = 1;
-					num = map[i][j];
-					isBuild = true;		
-					
-				}else {	// 높이가 낮아지는 경우
-					if(num - map[i][j] > 1 ||!isDown) {
-						isBuild = false;
-						break;
-					}
-					isDown = false;
-					isUp = false;
-					
-					cnt = 1;
-					num = map[i][j];
-					isBuild = false;
+					// 경사로 설치
+					for(k = j; k < j + X; ++k) build[i][k][0] = true;
+					h = board[i][j];
 				}
 			}
 			
-			if(isBuild) res++;
+			if(isBuild) ++ans;
 		}
-		return res;
+		
+		// 열
+		for(int j = 0; j < N; ++j) {
+			isBuild = true;
+			h = board[0][j];
+			
+			for(int i = 1; i < N; ++i) {
+				if(board[i][j] == h) continue;
+				
+				// 높아지는 경우
+				else if(board[i][j] > h) {
+					// 높이 차가 2 이상인 경우
+					if(board[i][j] - h > 1) {
+						isBuild = false; 
+						break;
+					}
+					
+					k = i - 1;
+					cnt = 0;
+					// i-1부터 뒤쪽에 L개의 동일 높이의 땅이 있는지 확인
+					while(k >= 0 && board[k][j] == h && !build[k][j][1]) {
+						++cnt;
+						--k;
+					}
+					
+					if(cnt < X) {
+						isBuild = false;
+						break;
+					}
+					
+					// 경사로 설치
+					for(k = i - X; k < i; ++k) build[k][j][1] = true;
+					h = board[i][j];
+				}
+				
+				// 낮아지는 경우
+				else if(board[i][j] < h) {
+					// 높이 차가 2 이상인 경우
+					if(h - board[i][j] > 1) {
+						isBuild = false; 
+						break;
+					}
+					k = i;
+					cnt = 0;
+					// i부터 뒤쪽에 L개의 동일 높이의 땅이 있는지 확인
+					while(k < N && board[k][j] == h-1 && !build[k][j][1]) {
+						++cnt;
+						++k;
+					}
+					
+					if(cnt < X) {
+						isBuild = false;
+						break;
+					}
+					
+					// 경사로 설치
+					for(k = i; k < i + X; ++k) build[k][j][1] = true;
+					h = board[i][j];
+				}
+			}
+			
+			if(isBuild) ++ans;
+		}
+		return ans;
 	}
 
 }
