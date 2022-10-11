7af68e2eeac0cbfde76873165e90728da7857966
Chanseok <ckstjr93@naver.com>
2022-10-11 15:49:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 119 ms, Memory: 26,476 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,80 +1,72 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Solution {
-	static int N, K;
-	static char[] number;
-	static String[] pws;
 	
+	static int N, K, len;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
 		StringTokenizer st;
 		
+		String numbers, password;
+		Iterator<String> iter;
+		
+		TreeSet<String> passSet = new TreeSet<>(new Comparator<String>() {
+			char c1, c2;
+			@Override
+			public int compare(String o1, String o2) {
+				for(int i = 0; i < len; ++i) {
+					c1 = o1.charAt(i); c2 = o2.charAt(i);
+					
+					if(c1 == c2) continue;
+					// 둘다 숫자이거나 둘다 문자일 때
+					if( (c1 >= '0' && c1 <= '9' && c2 >= '0' && c2 <= '9') 
+							||(c1 >= 'A' && c1 <= 'F' && c2 >= 'A' && c2 <= 'F')) {
+						return o2.compareTo(o1);
+					}
+					// 하나는 숫자,하나는 문자일 때
+					else {
+						if( c1 >= '0' && c1 <= '9') return 1;
+						else return -1;
+					}
+					
+				}
+				return 0;
+			}
+		});
+		
 		int T = Integer.parseInt(br.readLine());
+		
 		for(int tc = 1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 		
-			number = br.readLine().toCharArray();
-			sb.append("#" + tc + " " + simulation() + "\n");
+			numbers = br.readLine();
+			len = N / 4;
+			for(int i = 0; i < len; ++i)
+				numbers += numbers.charAt(i);
+			
+			passSet.clear();
+			for(int i = 0; i < len; ++i) {
+				for(int j = 0; j < 4; ++j) {
+					passSet.add(numbers.substring(i + j*len, i + j*len + len));
+				}
+			}
+			
+			iter = passSet.iterator();
+			for(int i = 0; i < K -1; ++i) iter.next();
+			password = iter.next();
+			
+			System.out.printf("#%d %d\n", tc, Integer.parseInt(password, 16));
 			
 		}// tc 종료
-		bw.write(sb.toString());
-		bw.close();
-	}
-
-	private static int simulation() {
-		pws = new String[N];
-		Arrays.fill(pws, "000");
-		int pw_cnt = 0;
-		int length = N/4;
-		
-		StringBuilder temp;
-		for(int start = 0; start < length; start++) {
-			
-			temp = new StringBuilder();
-			int point = start;
-			for(int cnt = 1; cnt <= N; cnt++) {
-				temp.append(number[point]);
-				if(cnt % length == 0) {
-					String str = temp.toString();
-					if(!DupCheck(str, pw_cnt)) pws[pw_cnt++] = str;
-					temp = new StringBuilder();
-				}
-				
-				if(++point >= N) point %= N;
-			}
-		}
-		
-		Arrays.sort(pws, (pw1, pw2) -> pw2.compareTo(pw1));
-
-		return HexToDec(pws[K-1]);
-		
-	}
-	private static boolean DupCheck(String str, int pw_cnt) {
-		
-		for(int i = 0; i < pw_cnt; i++) {
-			if(pws[i].compareTo(str) == 0) return true;
-		}
-		return false;
-	}
-	
-	private static int HexToDec(String num) {
-		int res = 0;
-		for(int i = 0, len = N/4; i < len; i++) {
-			char Hex = num.charAt(i);
-			if(Hex >= '0' && Hex <= '9') res += (Hex - '0') * Math.pow(16, len-1-i);  
-			else res += (Hex - 'A' + 10) * Math.pow(16, len-1-i);
-		}
-		return res;
 	}
 
 }
