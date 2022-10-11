c5c57752ee4fced417cca7de0b027cf6b420eeb8
Chanseok <ckstjr93@naver.com>
2022-10-11 20:06:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 142 ms, Memory: 26,552 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,37 +1,35 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static class Peak {
-		int x, y, k;
+	static class Point{
+		int x, y;
 
-		public Peak(int x, int y, int k) {
+		public Point(int x, int y) {
 			super();
 			this.x = x;
 			this.y = y;
-			this.k = k;
 		}
+		
 	}
-
-	static int N, K, max;
-	static int[][] map;
-	static boolean[][] visit;
+	
+	static int N, K, max, peek, height;
+	static int[][] board, cntBoard;
 	static int[] dx = { 0, 1, 0, -1 };
 	static int[] dy = { 1, 0, -1, 0 };
-
+	static boolean[][] visit;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
+		Queue<Point> peeks = new LinkedList<>();
+		
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
@@ -39,64 +37,67 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 
-			map = new int[N][N];
-
-			int height = 0;
-			Queue<Peak> peaks = new LinkedList<>(); // 최대 정점에 대한 좌표 구하기
-			for (int i = 0; i < N; i++) {
+			board = new int[N][N];
+			cntBoard = new int[N][N];
+			for(int i = 0; i < N; ++i) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-					if (map[i][j] >= height) {
-						if (map[i][j] > height)
-							peaks.clear();
-						height = map[i][j];
-						peaks.add(new Peak(i, j, K));
+				for(int j = 0; j < N; ++j) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			peeks.clear();
+			peek = -1;
+			for(int i = 0; i < N; ++i) {
+				for(int j = 0; j < N; ++j) {
+					height = board[i][j];
+					if(height >= peek) {
+						if(height > peek) {
+							peeks.clear();
+							peek = height;
+						}
+						peeks.offer(new Point(i, j));
 					}
 				}
-			} // 정보 입력 종료
-
-			max = 0;
-			int res = 0;
-			while (!peaks.isEmpty()) { // 각 정점에서 탐색 시작
-				Peak cur = peaks.poll();
-				visit = new boolean[N][N];
-				dfs(cur.x, cur.y, 1, true);
-				res = Math.max(res, max);
 			}
-			sb.append("#" + tc + " " + res + "\n");
-
+			
+			max = 0;
+			
+			visit = new boolean[N][N];
+			for(Point point : peeks) {
+				visit[point.x][point.y] = true;
+				dfs(point.x, point.y, K, 1);
+				visit[point.x][point.y] = false;
+			}
+			System.out.printf("#%d %d\n", tc, max);
+			
 		} // test-case 종료
 
-		bw.write(sb.toString());
-		bw.close();
 	}
 
-	private static void dfs(int x, int y, int length, boolean isK) {
-		visit[x][y] = true; // 방문처리
-
-		for (int d = 0, dSize = dx.length; d < dSize; d++) { // 현재 위치에서 4방향 탐색
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N || visit[nx][ny]) // 격자 밖과 방문 여부 확인
-				continue;
-
-			if (map[x][y] > map[nx][ny]) { // 이동하려는 곳이 낮은 숫자이면 이동
-				dfs(nx, ny, length + 1, isK);
-				
-			} else {
-				if (isK && map[x][y] + K > map[nx][ny]) { // 공사를 한 적이 없고 공사하여 이동할 수 있는지 판단
-					int temp = map[nx][ny]; 
-					map[nx][ny] = map[x][y] - 1;
-					dfs(nx, ny, length + 1, false);
-					map[nx][ny] = temp;
-					
-				}
+	private static void dfs(int x, int y, int K, int move) {
+		
+		int nx, ny;
+		int num = board[x][y];
+		int temp;
+		visit[x][y] = true;
+		for(int d = 0; d < 4; ++d) {
+			nx = x + dx[d]; ny = y + dy[d];
+			
+			if(nx < 0 || nx >= N || ny < 0 || ny >= N || visit[nx][ny] || board[nx][ny]-K >= num) continue;
+			
+			if(K != 0 && board[nx][ny] >= num) {
+				temp = board[nx][ny];
+				board[nx][ny] = num - 1;
+				dfs(nx, ny, 0, move + 1);
+				board[nx][ny] = temp;
+			}
+			else {
+				dfs(nx, ny, K, move + 1);
 			}
 		}
 		visit[x][y] = false;
-		max = Math.max(max, length); // 최대 이동 거리
+		max = Math.max(max, move);
+			
 	}
-
 }
