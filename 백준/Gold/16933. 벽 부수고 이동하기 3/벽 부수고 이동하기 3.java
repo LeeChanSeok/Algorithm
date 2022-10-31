6eaf86da9e86abe76a894afe621d42cc0339b3ad
Chanseok <ckstjr93@naver.com>
2022-10-31 22:56:19 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 2492 ms, Memory: 359556 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Point{
+		int x, y, k;
+
+		public Point(int x, int y, int k) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.k = k;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int[] dx = {-1, 1, 0, 0};
+		int[] dy = {0, 0, -1, 1};
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		char[][] board = new char[N][M];
+		for(int i = 0; i < N; ++i)
+			board[i] = br.readLine().toCharArray();
+		
+		int dist = simulation(N, M, K, dx, dy, board);
+		System.out.println(dist);
+	}
+
+	private static int simulation(int N, int M, int K, int[] dx, int[] dy, char[][] board) {
+		
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(0, 0, K+1));
+		
+		int[][][] visit = new int[N][M][2];
+		visit[0][0][0] = K+1;
+		
+		int dist = 1;
+		int qSize, nx, ny;
+		Point cur;
+		int night = 0;
+		
+		while(!q.isEmpty()) {
+			qSize = q.size();
+			while(qSize-- > 0){
+				cur = q.poll();
+				
+				if(cur.x == N -1 && cur.y == M-1) return dist;
+				
+				for(int d = 0; d < 4; ++d) {
+					nx = cur.x + dx[d];
+					ny = cur.y + dy[d];
+					
+					if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+					
+					// '0'으로 갈 때
+					if(board[nx][ny] == '0') {
+						if(cur.k > visit[nx][ny][night]) {
+							visit[nx][ny][night] = cur.k;
+							q.add(new Point(nx, ny, cur.k));
+						}
+					}
+					
+					// 밤일때
+					if((night&1) == 1) {
+						//제자리
+						if(cur.k > visit[cur.x][cur.y][night]) {
+							visit[cur.x][cur.y][night] = cur.k;
+							q.add(new Point(cur.x, cur.y, cur.k));
+						}
+					}else {//낮일때
+						// '0'으로 갈 때
+						if(board[nx][ny] == '1') {
+							 // '1' 으로 갈 때
+							if(cur.k-1 > visit[nx][ny][night]){
+								visit[nx][ny][night] = cur.k-1;
+								q.add(new Point(nx, ny, cur.k-1));
+							}
+						}
+						
+					}
+				
+				}
+			}
+			++dist;
+			night ^= 1;
+		}
+			
+		return -1;
+	}
+
+
+}
