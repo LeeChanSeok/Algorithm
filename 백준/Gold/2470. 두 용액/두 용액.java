8e39bc664e17051350c247415aaf47cb8aca7e2c
Chanseok <ckstjr93@naver.com>
2022-11-08 21:09:54 +0900

    [Gold V] Title: 두 용액, Time: 292 ms, Memory: 31908 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] water = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; ++i)
+			water[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(water);
+		int s = 0, e = N-1;
+		long sum;
+		long ans = Long.MAX_VALUE;
+		int[] pnt = new int[2];
+		int l, r;
+		
+		while(s < e) {
+			l = water[s]; r = water[e];
+			sum = l + r;
+			
+			if(sum < 0) ++s;
+			else --e;
+			
+			sum = sum < 0? -sum : sum;
+			
+			if(ans > sum) {
+				ans = sum;
+				pnt[0] = l; pnt[1] = r; 
+			}
+			
+		}
+
+		System.out.println(pnt[0] + " " + pnt[1]);
+	}
+
+}
