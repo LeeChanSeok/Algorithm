3a2ca3b5b91044a06290424c59410c938d5c6036
Chanseok <ckstjr93@naver.com>
2023-05-23 17:26:01 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 179 ms, Memory: 28,952 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -1,68 +1,64 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static final int T = 10;
-	static int N, M;
-	static List<Integer> pw;
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+		StringBuilder sb;
+		
 		StringTokenizer st;
-
-		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
+		int T = 10;
+		for(int tc = 1; tc <= T; ++tc) {
+			sb = new StringBuilder();
+			sb.append("#" + tc + " ");
+			int N = Integer.parseInt(br.readLine());
+			
+			List<Integer> list = new LinkedList<>();
+			
 			st = new StringTokenizer(br.readLine());
-			pw = new ArrayList<>(N);
-			for (int i = 0; i < N; i++)
-				pw.add(Integer.parseInt(st.nextToken()));
-
-			M = Integer.parseInt(br.readLine());
+			for(int i = 0; i < N; ++i) list.add(Integer.parseInt(st.nextToken()));
+			
+			int M = Integer.parseInt(br.readLine());
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < M; i++) {
-				char command = st.nextToken().charAt(0);
-
-				if (command == 'I') {
+			for(int i = 0; i < M; ++i) {
+				char cmd = st.nextToken().charAt(0);
+				
+				if(cmd == 'I') {
 					int x = Integer.parseInt(st.nextToken());
 					int y = Integer.parseInt(st.nextToken());
-
-					List<Integer> Add = new ArrayList<>(y);
-					while (y-- > 0)
-						Add.add(Integer.parseInt(st.nextToken()));
-					pw.addAll(x, Add);
-				} else if (command == 'D') {
+					for(int j = 0, idx = x; j < y; ++j, ++idx) {
+						list.add(idx, Integer.parseInt(st.nextToken()));
+					}
+				}else if(cmd == 'D') {
 					int x = Integer.parseInt(st.nextToken());
 					int y = Integer.parseInt(st.nextToken());
-
-					while (y-- > 0)
-						pw.remove(x);
-				} else {
+					for(int j = 0; j < y; ++j) list.remove(x);
+				}else if(cmd == 'A') {
 					int y = Integer.parseInt(st.nextToken());
-					
-					List<Integer> Add = new ArrayList<>(y);
-					while (y-- > 0)
-						Add.add(Integer.parseInt(st.nextToken()));
-					pw.addAll(pw.size() - 1, Add);
+					for(int j = 0; j < y; ++j) list.add(Integer.parseInt(st.nextToken()));
 				}
 			}
-
-			sb.append("#" + tc);
-			for (int i = 0; i < 10; i++)
-				sb.append(" " + pw.get(i));
+			
+			int i = 0;
+			for(int num : list) {
+				sb.append(num + " ");
+				if(++i == 10) break;
+			}
 			sb.append("\n");
-
+			
+			bw.write(sb.toString());
 		}
-		bw.write(sb.toString());
 		bw.close();
+
 	}
 
 }
