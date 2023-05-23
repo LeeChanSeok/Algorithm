103471b1f61e56d137bba8804a7143682fb77de7
Chanseok <ckstjr93@naver.com>
2023-05-23 17:25:13 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 151 ms, Memory: 28,552 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -1,97 +1,91 @@
+
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
+    static int[][] child;
+    static int[] parent;
 
-	static int V, E, v1, v2;
-	static int pTree[][], cTree[];
-	static List<Integer> v1_p, v2_p;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			v1 = Integer.parseInt(st.nextToken());
-			v2 = Integer.parseInt(st.nextToken());
-			
-			pTree = new int[V+1][2];
-			cTree = new int[V+1];
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < E; i++) {
-				int p = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-				
-				if(pTree[p][0] == 0) pTree[p][0]= c;
-				else pTree[p][1] = c;
-				cTree[c] = p;
-			}
-			
-			v1_p = new ArrayList<>(); 
-			v2_p = new ArrayList<>();
-			
-			int parent;
-			//v1 parent 찾기
-			parent = v1;
-			while(parent != 1) {
-				v1_p.add(parent);
-				parent = cTree[parent];
-			}
-			
-			//v1 parent 찾기
-			parent = v2;
-			while(parent != 1) {
-				v2_p.add(parent);
-				parent = cTree[parent];
-			}
-			
-			// 공통 부모 찾기
-			int root = 1;
-			boolean isFind = false;
-			for(int v1p : v1_p) {
-				for(int v2p: v2_p) {
-					if(v1p == v2p) {
-						root = v1p;
-						isFind = true;
-						break;
-					}
-				}
-				if(isFind) break;
-			}
-			
-			
-			int res = 1;
-			Queue<Integer> q = new LinkedList<>();
-			q.add(root);
-			while(!q.isEmpty()) {
-				int cur = q.poll();
-				
-				if(pTree[cur][0] != 0) {
-					q.add(pTree[cur][0]);
-					res++;
-				}
-				
-				if(pTree[cur][1] != 0) {
-					q.add(pTree[cur][1]);
-					res++;
-				}
-				
-			}
-			
-			
-			System.out.println("#" + tc + " " + root + " " + res);
-		}//tc종료
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; ++tc){
+            st = new StringTokenizer(br.readLine());
+
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int[][] child = new int[V+1][2];
+            int[] parent = new int[V+1];
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < E; ++i){
+                int f = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+
+                if(child[f][0] == 0) child[f][0] = t;
+                else child[f][1] = t;
+
+                parent[t] = f;
+
+            }
+
+            List<Integer> A_pList = new ArrayList<>();
+            List<Integer> B_pList = new ArrayList<>();
+
+            find_parents(parent, A_pList, A);
+            find_parents(parent, B_pList, B);
+
+            int ASize = A_pList.size();
+            int BSize = B_pList.size();
+
+            int i = ASize-1, j = BSize-1;
+
+            int comm = 0;
+            while((A_pList.get(i).intValue() == B_pList.get(j).intValue()) && i >= 0 && j >= 0){
+                comm = A_pList.get(i);
+                --i; --j;
+            }
+
+            int cnt = bfs(child, comm);
+            System.out.printf("#%d %d %d\n", tc, comm, cnt);
+
+        }
+    }
+
+    private static int bfs(int[][] child, int comm) {
+        int cnt = 1;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(comm);
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            if(child[node][0] != 0) {
+                q.offer(child[node][0]); ++cnt;
+            }
+            if(child[node][1] != 0){
+                q.offer(child[node][1]); ++cnt;
+            }
+        }
+        return cnt;
+    }
+
+    private static void find_parents(int[] parent, List<Integer> Plist, int num) {
+        int node = num;
+
+        while(node != 1){
+            Plist.add(node);
+            node = parent[node];
+        }
+        Plist.add(node);
+
+    }
 
 }
