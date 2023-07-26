2a3687922a0914050e511a2ad33bf79c86da94da
Chanseok <ckstjr93@naver.com>
2023-07-26 15:52:27 +0900

    [Gold II] Title: 피보나치 수 3, Time: 84 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int n;
+	
+	public static void main(String[] args) throws Exception {
+		input();
+		pro();
+	}
+
+	private static void pro() {
+		
+		int fibo1 = 0;
+		int fibo2 = 1; 
+		
+		for(int i = 2; i <= n; ++i) {
+			int temp = fibo2;
+			fibo2 = (fibo2 +fibo1) % 1000000;
+			fibo1 = temp;
+		}
+		
+		System.out.println(fibo2);
+	}
+
+	private static void input() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = (int)(Long.parseLong(br.readLine()) % 1500000);
+	}
+
+}
