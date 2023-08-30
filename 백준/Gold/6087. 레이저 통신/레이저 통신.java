9d794ad99a88699a4b4595a55fb89bcc95d0ff4f
Chanseok <ckstjr93@naver.com>
2023-08-31 01:00:15 +0900

    [Gold III] Title: 레이저 통신, Time: 324 ms, Memory: 22556 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.java
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.java
@@ -1,106 +1,112 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
-class Info{
-	int x, y;
-	int dir;
-	int turn;
-	
-	public Info(int x, int y, int dir, int turn) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.dir = dir;
-		this.turn = turn;
-	}
-	public Info(Info info) {
-		super();
-		this.x = info.x;
-		this.y = info.y;
-		this.dir = info.dir;
-		this.turn = info.turn;
-	}
-	
-}
 
 public class Main {
 
-	public static int min, W, H;
-	public static int[] dx = { -1, 0, 1, 0 };
-	public static int[] dy = { 0, 1, 0, -1 };
+	static class Info {
+		int pos, dir, turn;
 
+		public Info(int pos, int dir, int turn) {
+			super();
+			this.pos = pos;
+			this.dir = dir;
+			this.turn = turn;
+		}
+	}
 
-	public static void bfs(char[][] map, int[][] cntMap, Info[] info) {
-		int N = map.length;
-		int M = map[0].length;
-		
-		Queue<Info> q = new LinkedList<>();
-		q.add(new Info(info[0]));
-		cntMap[info[0].x][info[0].y]= 0; 
-		int nx, ny;
-		while(!q.isEmpty()) {
-			Info cur = q.poll();
-			if(cur.turn > min) continue;
-			
-			if(cur.x == info[1].x && cur.y == info[1].y ) {
-				min = Math.min(min, cntMap[cur.x][cur.y]);
-				continue;
-			}
-			for(int d = 0; d < dx.length; d++) {
-				nx = cur.x + dx[d];
-				ny = cur.y + dy[d];
-				
-				if(nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] =='*' ) continue;
-				
-				if(cur.dir != -1 && cur.dir == d &&cur.turn > cntMap[nx][ny]) continue;
-				if(cur.dir != -1 && cur.dir != d &&cur.turn+1 > cntMap[nx][ny]) continue;
-				
-				Info new_info;
-				if(cur.dir == -1) {
-					new_info = new Info(nx, ny, d, 0);
-				}else {
-					if(cur.dir == d)
-						new_info = new Info(nx, ny, d, cur.turn);
-					else
-						new_info = new Info(nx, ny, d, cur.turn+1);
-				}
-				cntMap[nx][ny] = new_info.turn;
-				q.add(new_info);
-			}
-			
+	static int INF = 987654321;
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+
+	static int w, h;
+	static int DELTA = 100;
+	static char[][] board;
+	static int[] c_pos = new int[2];
+
+	public static void main(String[] args) throws Exception {
+		input();
+		pro();
+	}
+
+	private static void pro() {
+		System.out.println(bfs());
+	}
+
+	private static int bfs() {
+
+		int sx = c_pos[0] / DELTA;
+		int sy = c_pos[0] % DELTA;
+		int ex = c_pos[1] / DELTA;
+		int ey = c_pos[1] % DELTA;
+
+		PriorityQueue<Info> pq = new PriorityQueue<>((p1, p2) -> p1.turn - p2.turn);
+		for (int d = 0; d < 4; ++d) {
+			pq.offer(new Info(sx * DELTA + sy, d, 0));
 		}
 
+		int[][][] visited = new int[h][w][4];
+		for (int i = 0; i < h; ++i) {
+			for (int j = 0; j < w; ++j) {
+				Arrays.fill(visited[i][j], INF);
+
+			}
+		}
+
+		while (!pq.isEmpty()) {
+			Info cur = pq.poll();
+
+			int x = cur.pos / DELTA;
+			int y = cur.pos % DELTA;
+			int dir = cur.dir;
+			int turn = cur.turn;
+
+			if (x == ex && y == ey)
+				return turn;
+
+			for (int d = -1; d <= 1; ++d) {
+				int ndir = (dir + 4 + d) % 4;
+				int nx = x + dx[ndir];
+				int ny = y + dy[ndir];
+
+				if (nx < 0 || nx >= h || ny < 0 || ny >= w)	continue;
+				if (board[nx][ny] == '*') continue;
+
+				int nturn = turn + Math.abs(d);
+				if (visited[nx][ny][ndir] <= nturn)	continue;
+
+				visited[nx][ny][dir] = nturn;
+				pq.offer(new Info(nx * DELTA + ny, ndir, nturn));
+
+			}
+
+		}
+
+		return -1;
 	}
-	
-	public static void main(String[] args) throws IOException {
+
+	private static void input() throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		W = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-		String str;
-		char[][] map = new char[H][W];
-		int[][] cntMap = new int[H][W];
-		Info[] info = new Info[2];
-		int icnt = 0;
-		for (int i = 0; i < H; i++) {
-			str = br.readLine();
-			for (int j = 0; j < W; j++) {
-				map[i][j] = str.charAt(j);
-				cntMap[i][j] = Integer.MAX_VALUE;
-				if (map[i][j] == 'C')
-					info[icnt++] = new Info(i, j, -1, 0);
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+
+		board = new char[h][w];
+		for (int i = 0; i < h; ++i) {
+			board[i] = br.readLine().toCharArray();
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < h; ++i) {
+			for (int j = 0; j < w; ++j) {
+				if (board[i][j] == 'C')
+					c_pos[cnt++] = i * DELTA + j;
 			}
 		}
-		min = Integer.MAX_VALUE;
-		bfs(map, cntMap, info);
-		System.out.println(min);
 	}
 
 }
