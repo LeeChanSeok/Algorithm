122fcd66380bf9b251da5336f6482a66a7fb38b4
Chanseok <ckstjr93@naver.com>
2023-09-20 03:49:30 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,8 @@
+select ANIMAL_ID,	NAME,
+case
+  when SEX_UPON_INTAKE LIKE 'Neutered%' then 'O'
+  when SEX_UPON_INTAKE LIKE 'Spayed%' then 'O'
+  else 'X'
+  end as 중성화
+from ANIMAL_INS
+order by 1
