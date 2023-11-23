cdc5d1829c4f22ba688e0a8ba4b8f724ed751e95
Chanseok <ckstjr93@naver.com>
2023-11-23 20:56:54 +0900

    [Silver IV] Title: 토너먼트, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int ans = 0;
+		while(a != b) {
+			a = (a+1) / 2;
+			b = (b+1) / 2;
+			ans++;
+		}
+		
+		System.out.println(ans);
+	}
+
+}
