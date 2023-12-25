d55949a2fc028333bf64e910fcd8ca26317a25a1
Chanseok <ckstjr93@naver.com>
2023-12-25 17:07:01 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		arr = new int[n+1];
+		for(int i = 1; i <= n; ++i) {
+			arr[i] = i;
+		}
+		
+		for(int i = 0; i < m; ++i) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			reverse(a, b);
+		}
+		
+		for(int i = 1; i <= n; ++i){
+			sb.append(arr[i] + " "); 
+		}
+		
+		System.out.println(sb);
+	}
+
+	private static void reverse(int a, int b) {
+
+		
+		while(a < b) {
+			int temp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = temp;
+
+			a++; b--;
+		}
+		
+	}
+}
