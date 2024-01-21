6fa386bbac8284b9bdbf2101d6d8f9c1e2025e39
Chanseok <ckstjr93@naver.com>
2024-01-21 16:47:38 +0900

    [Silver V] Title: 창문 닫기, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		for(int i = 1; i * i <= n; ++i) ++cnt;
+		
+		System.out.println(cnt);
+
+	}
+
+}
