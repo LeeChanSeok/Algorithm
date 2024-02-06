72975f07878b24c69e430c60f95bf19d35d2de9a
Chanseok <ckstjr93@naver.com>
2024-02-06 19:07:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 2580 ms, Memory: 117832 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, m, r;
+	static List<Integer>[] graph;
+	static int[] visited;
+	static int step;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+
+		visited = new int[n + 1];
+
+		graph = new List[n + 1];
+		for (int i = 1; i <= n; ++i) {
+			graph[i] = new LinkedList<>();
+		}
+
+		for (int i = 0; i < m; ++i) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+		
+		for (int i = 1; i <= n; ++i) {
+			Collections.sort(graph[i]);
+		}
+
+		step = 1;
+		dfs(r);
+		
+		for(int i = 1; i <= n; ++i) {
+			System.out.println(visited[i]);
+		}
+	}
+
+	private static void dfs(int r) {
+		
+		visited[r] = step++;		
+		
+		for(int next : graph[r]) {
+			if(visited[next] != 0) continue;
+			dfs(next);
+		}
+
+	}
+
+}
