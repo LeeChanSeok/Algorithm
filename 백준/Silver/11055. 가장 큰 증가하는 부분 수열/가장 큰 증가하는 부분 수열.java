e389f9981c76d94131d5bb76c4aea49c70500a71
Chanseok <ckstjr93@naver.com>
2024-03-07 20:23:25 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 96 ms, Memory: 11928 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int i, j;
-		int n=sc.nextInt();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
 		int a[] = new int[n+1];
 		int d[] = new int[n+1];
 		
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1;i <= n; i++) a[i] = Integer.parseInt(st.nextToken());
+		
 		int max = 0; 
-		
-		for(i=1;i<=n;i++) a[i] = sc.nextInt();
-		
-		for(i=1;i<=n;i++){
+		for(int i = 1; i <= n; i++){
 			d[i] = a[i];
-			for(j=1;j<i;j++)
+			for(int j = 1; j < i; j++)
 				if(a[j]<a[i] && d[i] < d[j]+a[i]) d[i] = d[j] + a[i];
 			if(max < d[i]) max = d[i];
 		}
