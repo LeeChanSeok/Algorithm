9d78ca6d4bd23aa7081cd16c6be26ef765cb3076
Chanseok <ckstjr93@naver.com>
2024-05-12 18:39:57 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 244 ms, Memory: 14860 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, k;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; ++i) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		selection_sort();
+	}
+
+	private static void selection_sort() {
+
+		int change = 0;
+		for (int last = n - 1; last >= 1; --last) {
+
+			int idx = 0;
+			int max = arr[idx];
+
+			for (int j = 1; j <= last; ++j) {
+				if (arr[j] >= max) {
+					idx = j;
+					max = arr[j];
+				}
+			}
+
+			if (last != idx) {
+				++change;
+
+				int temp = arr[last];
+				arr[last] = max;
+				arr[idx] = temp;
+
+				if (change == k) {
+					System.out.println(arr[idx] + " " + arr[last]);
+					return;
+				}
+			}
+
+		}
+
+		System.out.println(-1);
+	}
+
+}
