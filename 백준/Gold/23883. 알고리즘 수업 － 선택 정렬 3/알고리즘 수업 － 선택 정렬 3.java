f4cf797992edaaa18ba8e0cdd70187d32604042e
Chanseok <ckstjr93@naver.com>
2024-05-14 06:04:36 +0900

    [Gold IV] Title: 알고리즘 수업 - 선택 정렬 3, Time: 1204 ms, Memory: 121908 KB -BaekjoonHub

---
+++ 백준/Gold/23883. 알고리즘 수업 － 선택 정렬 3/알고리즘 수업 － 선택 정렬 3.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Data implements Comparable<Data> {
+		int num, pos;
+
+		public Data(int num, int pos) {
+			super();
+			this.num = num;
+			this.pos = pos;
+		}
+
+		@Override
+		public int compareTo(Data o) {
+			return o.num - this.num;
+		}
+
+	}
+
+	static int n, k;
+	static int[] arr;
+	static PriorityQueue<Data> pq;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+		pq = new PriorityQueue<>();
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; ++i) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			pq.offer(new Data(arr[i], i));
+		}
+
+		selection_sort();
+	}
+
+	private static void selection_sort() {
+
+		int change = 0;
+		for (int last = n - 1; last >= 1; --last) {
+
+			Data p = null;
+			while ((p = pq.poll()).pos > last)
+				;
+
+			if (p.pos != last) {
+				++change;
+
+				Data q = new Data(arr[last], p.pos);
+				arr[p.pos] = arr[last];
+				arr[last] = p.num;
+				pq.offer(q);
+
+				if (change == k) {
+					System.out.println(arr[p.pos] + " " + arr[last]);
+					return;
+				}
+			}
+
+		}
+
+		System.out.println(-1);
+	}
+
+}
