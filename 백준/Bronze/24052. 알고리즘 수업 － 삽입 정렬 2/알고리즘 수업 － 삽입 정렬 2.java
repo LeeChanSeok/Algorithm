023ff4ed061c2c5a6eda846bddbebaf5cf082304
Chanseok <ckstjr93@naver.com>
2024-05-19 18:26:00 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 2, Time: 204 ms, Memory: 17176 KB -BaekjoonHub

---
+++ 백준/Bronze/24052. 알고리즘 수업 － 삽입 정렬 2/알고리즘 수업 － 삽입 정렬 2.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, k;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; ++i) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		insertion_sort();
+	}
+
+	private static void insertion_sort() {
+		StringBuilder sb = new StringBuilder();
+		int change = 0;
+		for (int i = 1; i < n; ++i) {
+			int loc = i - 1;
+			int newItem = arr[i];
+
+			while (loc >= 0 && newItem < arr[loc]) {
+				++change;
+				arr[loc + 1] = arr[loc];
+				--loc;
+
+				if (change == k) {
+					for (int num : arr) {
+						sb.append(num + " ");
+					}
+					System.out.println(sb);
+					return;
+				}
+			}
+
+			if (loc + 1 != i) {
+				++change;
+				arr[loc + 1] = newItem;
+				if (change == k) {
+					for (int num : arr) {
+						sb.append(num + " ");
+					}
+					System.out.println(sb);
+					return;
+				}
+
+			}
+
+		}
+
+		System.out.println("-1");
+	}
+
+}
