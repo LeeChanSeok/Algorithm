934fceaa43494621664ba176b0b1e5dd51233121
Chanseok <ckstjr93@naver.com>
2024-06-07 06:06:03 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2476 ms, Memory: 715680 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+
+		Deque<int[]> q = new ArrayDeque<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			while (!q.isEmpty() && q.peekLast()[0] > num)
+				q.pollLast();
+
+			q.offer(new int[] { num, i });
+			if (q.peek()[1] < i - (l - 1)) {
+				q.poll();
+			}
+			
+			bw.write(q.peek()[0] + " ");
+		}
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
