46db010c739e1a02f10b11201f2899bd19420789
Chanseok <ckstjr93@naver.com>
2024-08-20 11:22:54 +0900

    [Silver I] Title: 두 박스, Time: 72 ms, Memory: 11564 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15973. 두 박스/두 박스.java
+++ 백준/Silver/15973. 두 박스/두 박스.java
@@ -4,8 +4,20 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	static class Pnt {
+		int x, y;
+
+		public Pnt(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+
+	}
+
 	static class Rec {
 		int x1, y1, x2, y2;
+		Pnt lu, ru, ld, rd;
 
 		public Rec(int x1, int y1, int x2, int y2) {
 			super();
@@ -13,6 +25,11 @@ public class Main {
 			this.y1 = y1;
 			this.x2 = x2;
 			this.y2 = y2;
+
+			lu = new Pnt(x1, y2);
+			ru = new Pnt(x2, y2);
+			ld = new Pnt(x1, y1);
+			rd = new Pnt(x2, y1);
 		}
 
 	}
@@ -38,24 +55,58 @@ public class Main {
 		y2 = Integer.parseInt(st.nextToken());
 		q = new Rec(x1, y1, x2, y2);
 
-		String ans = "NULL";
-		if (p.x1 == q.x2 || p.x2 == q.x1) {
-			if (p.y1 == q.y2 || p.y2 == q.y1)
-				ans = "POINT";
-			else if ((p.y2 - p.y1) + (q.y2 - q.y1) > Math.max(q.y2 - p.y1, p.y2 - q.y1))
-				ans = "LINE";
-			else
-				ans = "NULL";
-		} else if ((p.x2 - p.x1) + (q.x2 - q.x1) > Math.max(q.x2 - p.x1, p.x2 - q.x1)) {
-			if (p.y1 == q.y2 || p.y2 == q.y1)
-				ans = "LINE";
-			else if ((p.y2 - p.y1) + (q.y2 - q.y1) > Math.max(q.y2 - p.y1, p.y2 - q.y1))
-				ans = "FACE";
-			else
-				ans = "NULL";
-		}
+		String ans = "FACE";
 
+		if (POINT(p, q) || POINT(q, p))
+			ans = "POINT";
+		if (LINE(p, q) || LINE(q, p))
+			ans = "LINE";
+		if (NULL(p, q) || NULL(q, p))
+			ans = "NULL";
 		System.out.println(ans);
+	}
+
+	private static boolean NULL(Rec p, Rec q) {
+
+		if (p.lu.y < q.ld.y || p.ld.y > q.lu.y || p.lu.x > q.ru.x || p.ru.x < q.lu.x)
+			return true;
+
+		return false;
+	}
+
+	private static boolean LINE(Rec p, Rec q) {
+
+		if (isLine(p.lu.x, p.ru.x, q.ld.x, q.rd.x, p.lu.y, q.ld.y)
+				|| isLine(p.ld.x, p.rd.x, q.lu.x, q.ru.x, p.lu.y, q.ld.y)
+				|| isLine(p.ld.y, p.lu.y, q.rd.y, q.ru.y, p.lu.x, q.ru.x)
+				|| isLine(p.rd.y, p.ru.y, q.ld.y, q.ru.y, p.ru.x, q.lu.x))
+			return true;
+
+		return false;
+	}
+
+	private static boolean isLine(int p1, int p2, int q1, int q2, int p, int q) {
+
+		if (p != q)
+			return false;
+
+		if ((p1 < q2 && q2 < p2) || (p1 < q1 && q1 < p2) || (q1 <= p1 && p2 <= q2))
+			return true;
+
+		return false;
+	}
+
+	private static boolean POINT(Rec p, Rec q) {
+
+		if (isSame(p.lu, q.rd) || isSame(p.ru, q.ld) || isSame(p.ld, q.ru) || isSame(p.rd, q.lu))
+			return true;
+		return false;
+	}
+
+	private static boolean isSame(Pnt p1, Pnt p2) {
+		if (p1.x == p2.x && p1.y == p2.y)
+			return true;
+		return false;
 	}
 
 }
