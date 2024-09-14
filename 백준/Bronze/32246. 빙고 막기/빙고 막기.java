c3dd7aeadef5dc976e29b6d7aca2decd9b848297
Chanseok <ckstjr93@naver.com>
2024-09-14 16:28:41 +0900

    [Bronze III] Title: 빙고 막기, Time: 92 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n == 2)
+			System.out.println(3);
+		else
+			System.out.println(n);
+
+	}
+
+}
