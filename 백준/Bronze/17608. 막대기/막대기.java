fa2a5670fc2050b9ee4afda167ff70267831cbff
Chanseok <ckstjr93@naver.com>
2024-10-09 15:41:07 +0900

    [Bronze II] Title: 막대기, Time: 600 ms, Memory: 97108 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -14,32 +14,32 @@ public class Main {
 		}
 
 		// ver1
-//		{
-//			int max = arr[N - 1];
-//			int ans = 1;
-//			for (int i = N - 2; i >= 0; --i) {
-//				if (arr[i] > max) {
-//					max = arr[i];
-//					++ans;
-//				}
-//			}
-//			System.out.println(ans);
-//		}
+		{
+			int max = arr[N - 1];
+			int ans = 1;
+			for (int i = N - 2; i >= 0; --i) {
+				if (arr[i] > max) {
+					max = arr[i];
+					++ans;
+				}
+			}
+			System.out.println(ans);
+		}
 
 		// ver2
-		{
-			Stack<Integer> stack = new Stack<>();
-			for (int i = 0; i < N; ++i) {
-				int num = arr[i];
-
-				while (!stack.isEmpty() && stack.peek() <= num) {
-					stack.pop();
-				}
-				stack.push(num);
-			}
-
-			System.out.println(stack.size());
-		}
+//		{
+//			Stack<Integer> stack = new Stack<>();
+//			for (int i = 0; i < N; ++i) {
+//				int num = arr[i];
+//
+//				while (!stack.isEmpty() && stack.peek() <= num) {
+//					stack.pop();
+//				}
+//				stack.push(num);
+//			}
+//
+//			System.out.println(stack.size());
+//		}
 
 	}
 
